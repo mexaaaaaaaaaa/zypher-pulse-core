d9993d5d43edc84a047a22d0cb968b10ce0b905d
!           M𝑒𝓍𝒶𝒶𝒶𝒶𝒶🚬 <theghoxt69@gmail.com>
2026-05-28 15:11:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,3 +13,10 @@ export default defineConfig({
     server: { entry: "server" },
   },
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [tsConfigPaths(), react()],
+})
